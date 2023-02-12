f723c3a34fee14f73ed86bbd094f0b15dec7c7f2
Proximacentaurion <cristiancvn63@gmail.com>
2023-02-12 18:34:32 -0500

    Se crean los atributos de la clase Paciente, el constructor, sus geters y seters
    Se crean los atributos de la clase Paciente, el constructor, sus geters y seters

--- SistemaMedico/SistemaMedico/src/main/java/com/SistemaMedico/SistemaMedico/Paciente.java
+++ SistemaMedico/SistemaMedico/src/main/java/com/SistemaMedico/SistemaMedico/Paciente.java
@@ -1,4 +1,38 @@
 package com.SistemaMedico.SistemaMedico;
 
 public class Paciente {
+
+    private int cedula;
+    private String nombre;
+    private HistoriaClinica historia;
+
+    public Paciente (int cedula, String nombre, HistoriaClinica historia){
+        this.cedula=cedula;
+        this.nombre=nombre;
+        this.historia=historia;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public HistoriaClinica getHistoria() {
+        return historia;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHistoria(HistoriaClinica historia) {
+        this.historia = historia;
+    }
 }
